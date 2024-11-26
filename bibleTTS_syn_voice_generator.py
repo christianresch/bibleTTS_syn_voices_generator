d45fd8e755509add6a75c17b94ad6f4015338d25
Christian <christianresch@mailbox.org>
2024-11-26 18:54:19 -0500

    Added language and model as options for the command line

--- bibleTTS_syn_voice_generator.py
+++ bibleTTS_syn_voice_generator.py
@@ -6,7 +6,7 @@ import argparse
 
 class BibleTTSGenerator():
 
-    def __init__(self, csv: str, language: str = "hausa"):
+    def __init__(self, csv: str, language: str = None, model: str = None):
         self.data = pd.read_csv(csv,
                          header=0,
                          usecols=['Unique ID', 'Sentence (Translation in Target Language )'],
@@ -14,17 +14,25 @@ class BibleTTSGenerator():
 
         self.data = self.data.rename(columns={'Unique ID': 'unique_ID', 'Sentence (Translation in Target Language )': 'sentence'})
 
-        self.language = language
+        if language is None:
+            self.language = "hausa"
+        else:
+            self.language = language
 
         os.makedirs(os.path.dirname(f"{language}/"), exist_ok=True)
 
-    def generate_voice(self, n: Optional[int] = None, model: str = "tts_models/hau/openbible/vits"):
+        if model is None:
+            self.model = "tts_models/hau/openbible/vits"
+        else:
+            self.model = model
+
+    def generate_voice(self, n: Optional[int] = None):
         if n is not None:
             for row in self.data[:n].itertuples():
-                self.__call_tts__(unique_ID=row.unique_ID, sentence=row.sentence, model=model)
+                self.__call_tts__(unique_ID=row.unique_ID, sentence=row.sentence, model=self.model)
         else:
             for row in self.data.itertuples():
-                self.__call_tts__(unique_ID=row.unique_ID, sentence=row.sentence, model=model)
+                self.__call_tts__(unique_ID=row.unique_ID, sentence=row.sentence, model=self.model)
 
 
     def __call_tts__(self, unique_ID: str, sentence: str, model: str):
@@ -49,12 +57,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some input variables.")
     parser.add_argument("--csv", type=str, required=True,help="Path to csv file to use")
     parser.add_argument("--n", type=int, required=False, help="Number of rows to use")
-    #TODO Add model and language as optional arguments
+    parser.add_argument("--language", type=int, required=False, help="Language in which the audio is to be created. Default: Hausa")
+    parser.add_argument("--model", type=int, required=False, help="Which Coqui TTS model to use. Default: tts_models/hau/openbible/vits")
 
     # Parse the arguments
     args = parser.parse_args()
 
-    syn_voice_generator = BibleTTSGenerator(csv=args.csv)
+    syn_voice_generator = BibleTTSGenerator(csv=args.csv, language=args.language, model=args.model)
 
     syn_voice_generator.generate_voice(n=args.n)
 
